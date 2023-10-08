2a6e3d12110bd409addf01a83bed0536ae760331
GouravBakshi <gouravbakshi1k1@gmail.com>
2023-10-08 15:13:53 +0530

    Updates

---
+++ precache-manifest.874389e3b2b9280d8badd6809aec6f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a9dd516ef1ff162c47581abc8da168",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5801be311b3ff41d1e9",
+    "url": "/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5801be311b3ff41d1e9",
+    "url": "/static/js/main.b822a84c.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
